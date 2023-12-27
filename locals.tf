73c70ab910ddd8b941eca7f3266b598b7bc8c285
TheModeler99 <nyuykimo@gmail.com>
2023-12-27 09:19:33 -0500

    edited variables

--- locals.tf
+++ locals.tf
@@ -1,8 +1,8 @@
 locals {
   common_tags = {
-    company    = "jjtech"
-    owner      = "jjtech DevOps Team"
-    team-email = "team-devops@jjtech.com"
+    company    = "Bluejay.internal"
+    owner      = "Gabinator"
+    team-email = "devops-team@bluejay.com"  #"team-devops@jjtech.com"
     time       = formatdate("DD MMM YYYY hh:mm ZZZ", timestamp())
   }
 
