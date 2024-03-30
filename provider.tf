67862e648ba765cb80ee21f683ee8800a9180eef
TheModeler99 <nyuykimo@gmail.com>
2024-03-30 01:27:37 -0400

    adding provider.tf code

--- provider.tf
+++ provider.tf
@@ -1,3 +1,14 @@
+# We need to declare aws terraform provider. You may want to update the aws region
+
+terraform {  
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.67.0"
+    }
+  }
+}
+
 provider "aws" {
-  region = "us-east-1"  
+  region = "us-east-1"
 }
