20baf0687027bc073caaaba4565b90e2257ca510
TheModeler99 <nyuykimo@gmail.com>
2024-03-30 03:37:38 -0400

    edited scripts

--- scripts/userdata.sh
+++ scripts/userdata.sh
@@ -27,8 +27,8 @@ rm -rf node_exporter-1.0.1.linux-amd64.tar.gz node_exporter-1.0.1.linux-amd64
 
 # setup the node-exporter dependencies
 sudo yum install git -y
-sudo git clone -b installations https://github.com/cvamsikrishna11/devops-fully-automated.git /tmp/devops-fully-automated
-sudo cp /tmp/devops-fully-automated/prometheus-setup-dependencies/node-exporter.service /etc/systemd/system/node-exporter.service
+sudo git clone https://github.com/TheModeler99/fully-automated-infras-project.git /tmp/fully-automated-infras-project
+sudo cp /tmp/fully-automated-infras-project/node-exporter.service /etc/systemd/system/node-exporter.service
 
 sudo systemctl daemon-reload
 sudo systemctl enable node-exporter
